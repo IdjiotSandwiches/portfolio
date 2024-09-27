857585e5b20428099d758d4834a6f576a17b929d
Vincen <vincenhayate123@gmail.com>
2024-09-27 22:54:11 +0700

    Add logos & update components

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import * as React from "react";
 import NavigationBar from "@/app/ui/navigation-bar";
 import { Timeline } from "@/app/ui/timeline";
@@ -9,19 +8,55 @@ export default function Home() {
     <>
       <NavigationBar />
       <div className="md:px-40">
-        <section id="about" className="min-h-screen">
+        <section id="about" className="min-h-screen flex items-center gap-4">
           <div>
-            
+            <h3 className="text-2xl">Hi there.</h3>
+            <h2 className="text-3xl font-semibold">I'm Vincen</h2>
           </div>
+        </section>
+        <section id="education" className="min-h-screen">
           <div>
-            <h3 className="text-2xl font-semibold mb-4">Education</h3>
+            <h3 className="text-3xl font-semibold mb-4">Education & Work Experience</h3>
             <ol className="relative border-s border-gray-200 dark:border-gray-700 mb-4">
-              <Timeline time="2019 - 2022" studyPlace="SMAN 19 Jakarta" />
-              <Timeline time="2022 - Present" studyPlace="Bina Nusantara University" />
+              <Timeline 
+                time="2019 - 2022" 
+                studyPlace="SMAN 19 Jakarta" 
+                desc="As a student"
+              />
+              <Timeline 
+                time="2022 - Present" 
+                studyPlace="Bina Nusantara University" 
+                desc="As a student"
+                path={[
+                  'svg/html.svg',
+                  'svg/css.svg',
+                  'svg/js.svg',
+                  'svg/php.svg',
+                  'svg/laravel.svg',
+                  'svg/mysql.svg',
+                  'svg/figma.svg',
+                  'svg/python.svg',
+                ]}
+              />
+              <Timeline 
+                time="2024 - Present" 
+                studyPlace="Associate Member of Bina Nusantara IT Division" 
+                desc="Working as web developer." 
+                path={[
+                  'svg/html.svg',
+                  'svg/css.svg',
+                  'svg/js.svg',
+                  'svg/php.svg',
+                  'svg/laravel.svg',
+                  'svg/mysql.svg',
+                  'svg/jquery.svg'
+                ]}
+              />
             </ol>
           </div>
         </section>
         <section id="experience" className="min-h-screen">
+          <h1 className="text-3xl font-semibold mb-4">Project Experience</h1>
           <div className="flex gap-4">
             <ProjectCard srcImage="/Logo.png" altImage="Logo" title="Logo" desc="Branding Logo" />
             {/* <ProjectCard /> */}
--- app/ui/timeline.tsx
+++ app/ui/timeline.tsx
@@ -1,16 +1,34 @@
+"use client";
+
 import * as React from 'react';
+import Image from 'next/image';
 
 interface TimelineProps {
 	time: string;
 	studyPlace: string;
+	desc?: string;
+	path?: string[];
 }
 
-export const Timeline: React.FC<TimelineProps> = ({ time, studyPlace }) => {
+export const Timeline: React.FC<TimelineProps> = ({ time, studyPlace, desc, path }) => {
   return (
 	<li className="mb-10 ms-4">
 		<div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-		<time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">{ time }</time>
-		<h3 className="text-lg font-semibold text-gray-900 dark:text-white">{ studyPlace }</h3>
+		<time className="mb-1 text-md font-normal leading-none text-gray-400 dark:text-gray-500">{ time }</time>
+		<h3 className="text-xl font-semibold text-gray-900 dark:text-white">{ studyPlace }</h3>
+		<p className='text-lg mb-2 text-gray-500 dark:text-gray-600'>{ desc }</p>
+		<div className='flex gap-2'>
+			{path?.map((item) => {
+				return (
+					<Image 
+						src={item}
+						alt={item}
+						width={45}
+						height={45}
+					/>
+				);
+			})}
+		</div>
 	</li>             
   );
 }
