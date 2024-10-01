e9df3fbad0bcc4a63fcd4c67498cbe2ebcdccaee
Vincen <vincenhayate123@gmail.com>
2024-10-01 16:36:06 +0700

    Remove unnecessary package

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,5 @@
 import type { Config } from "tailwindcss";
 import { nextui } from "@nextui-org/react";
-import flowbite from "flowbite-react/tailwind";
 
 const config: Config = {
   content: [
@@ -8,7 +7,6 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-    flowbite.content(),
   ],
   theme: {
   	extend: {
@@ -119,7 +117,6 @@ const config: Config = {
         },
       },
     }),
-    flowbite.plugin(),
     require('tailwind-scrollbar'),
     require("tailwindcss-animate"),
 ],
