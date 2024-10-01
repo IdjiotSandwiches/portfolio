a39c2d62813c05fa6ca64e9793d0952817564d01
Vin <124791055+IdjiotSandwiches@users.noreply.github.com>
2024-10-01 16:42:48 +0700

    Nav fix (#14)
    * Change color for dark mode
    
    * Add text color in body
    
    ---------
    
    Co-authored-by: Vincen <vincenhayate123@gmail.com>

--- app/ui/navigation/nav-links.tsx
+++ app/ui/navigation/nav-links.tsx
@@ -9,7 +9,7 @@ export const NavLinks: React.FC<NavLinksProps> = ({ name, path }) => {
 	return (
 		<NavbarItem>
 			<Link 
-				className="font-medium hover:text-primary text-gray-900 transition-colors 0.01s"
+				className="font-medium hover:text-primary transition-colors 0.01s"
 				color="foreground" 
 				href={ path }
 			>
