d1e65c4caed73c89a54b7cf32de238f2aa969c7e
Vincen <vincenhayate123@gmail.com>
2024-10-02 11:54:29 +0700

    Revert timeline structure

--- app/ui/timeline/timeline-icons.tsx
+++ app/ui/timeline/timeline-icons.tsx
@@ -6,20 +6,22 @@ interface TimelineIconsProps {
 
 export const TimelineIcons: React.FC<TimelineIconsProps> = ({ icons }) => {
 	return (
-		<div className="chrono-icons">
+		<>
 			{icons.map((Icon, index) => {
 				return (
-					<div 
-						key={`${Icon.name}-${index}`}
-						className="border-3 border-amber-600 bg-white w-full h-full rounded-full flex justify-center items-center"
-					>
-						<Icon 
-							size={30} 
-							className="fill-amber-600"
-						/>
-					</div>
+					<>
+						<div 
+							key={`${Icon.name}-${index}`}
+							className="border-3 border-amber-600 bg-white w-full h-full rounded-full flex justify-center items-center"
+						>
+							<Icon 
+								size={30} 
+								className="fill-amber-600"
+							/>
+						</div>
+					</>
 				);
 			})}
-		</div>
+		</>
 	);
 };
--- app/ui/timeline/timeline.tsx
+++ app/ui/timeline/timeline.tsx
@@ -3,6 +3,7 @@ import { Chrono } from "react-chrono";
 import { IconType } from "react-icons/lib";
 import { TimelineIcons } from "@/app/ui/timeline/timeline-icons";
 import { TimelineContents } from "@/app/ui/timeline/timeline-contents";
+import { LogoContainer } from "@/app/ui/logo-container";
 
 interface TimelineProps {
 	items: {
@@ -32,8 +33,38 @@ export const Timeline: React.FC<TimelineProps> = ({ items, mode, icons }) => {
 				secondary: "rgb(180 83 9)",
 			}}
 		>
-			<TimelineIcons icons={icons} />
-			<TimelineContents items={items} />
+			<div className="chrono-icons">
+				{icons.map((Icon, index) => {
+					return (
+						<div 
+							key={`${Icon.name}-${index}`}
+							className="border-3 border-amber-600 bg-white w-full h-full rounded-full flex justify-center items-center"
+						>
+							<Icon 
+								size={30} 
+								className="fill-amber-600"
+							/>
+						</div>
+					);
+				})}
+			</div>
+			{items.map((item, index) => {
+				return (
+					<div key={`${item.place}-${index}`} className="w-full scrollbar-hide">
+						<div className="flex flex-col flex-wrap gap-4">
+							<h3 className="font-normal text-gray-400">{item.time}</h3>
+							<div>
+								<h2 className="font-medium text-gray-900 text-xl">{item.place}</h2>
+								<p className="text-gray-500">{item.desc}</p>
+							</div>
+							<div>
+								<h4 className="font-medium text-secondary">{item.extraDesc}</h4>
+								<LogoContainer path={item.logo}/>
+							</div>
+						</div>
+					</div>
+				);
+			})}
 		</Chrono>
 	);
 }
