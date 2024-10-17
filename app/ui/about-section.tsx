d8b1bc2906b674a04013741203e1e2531112d875
Vin <124791055+IdjiotSandwiches@users.noreply.github.com>
2024-10-17 15:44:31 +0700

    Add Profile & CV (#28)
    * Add own photo
    
    * Add download CV
    
    ---------
    
    Co-authored-by: Vincen <vincenhayate123@gmail.com>

--- app/ui/about-section.tsx
+++ app/ui/about-section.tsx
@@ -1,11 +1,11 @@
-import Image from "next/image";
+import { Image } from "@nextui-org/react";
 
 export default function AboutSection() {
 	return (
 		<section id="about" className="pt-20 min-h-[calc(100vh-40vh)] text-gray-900">
         <h1 className="drop-shadow-sm text-5xl text-primary text-center font-medium mb-8">About Me</h1>
 		  <div className="flex flex-col md:flex-row items-center md:justify-between lg:mx-24 gap-4">
-			<div className="flex flex-col flex-wrap gap-2 text-center md:text-left text-lg">
+			<div className="flex flex-col flex-wrap gap-2 text-center md:text-left text-lg w-2/3">
 				<p>
 					{`I'm a Computer Science major at Bina Nusantara University, currently enrolled in an Artificial Intelligence course. I enjoy building websites, but I'm currently focused on Deep Learning and learning how to use TensorFlow. I also stay updated on state-of-the-art architectures in Artificial Intelligence.`}
 				</p>
@@ -13,13 +13,13 @@ export default function AboutSection() {
 					{`"Trying to do better" -some spiderman.`}
 				</p>
 			</div>
-			<Image 
-				src="/Logo.png"
-				alt="photos"
-				width={200}
-				height={200}
-				className="rounded-full"
-			/>
+			<div className="w-1/3">
+				<Image 
+					src="/profile-outline.png"
+					alt="photos"
+					className="rounded-full w-auto outline-primary outline object-cover"
+				/>
+			</div>
 		  </div>
       </section>
 	);
--- app/ui/landing-page-section.tsx
+++ app/ui/landing-page-section.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function LandingPageSection() {
 	return (
@@ -7,6 +8,16 @@ export default function LandingPageSection() {
             <h3 className="text-secondary text-2xl">{`Hi there!`}</h3>
             <h2 className="text-primary text-4xl font-medium">{`I'm Vincen`}</h2>
             <p className="text-gray-500 text-lg">A Computer Science major and Associate Member of the IT Division at Bina Nusantara University</p>
+            <div className="flex justify-center lg:justify-end pt-4">
+               <Link 
+                  download="Vincen - CV"
+                  href="/cv.pdf"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  className="rounded-lg bg-primary border-2 p-2 px-6 hover:bg-secondary text-white text-lg">
+                  <p>Download CV</p>
+               </Link>
+            </div>
          </div>
          <Image 
             className="w-5/6 lg:w-1/2"
